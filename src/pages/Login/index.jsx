c77cfef64c875dd8036629f235b99308134c2c8f
jingjingcai <jcai28@usfca.edu>
2023-10-06 18:00:15 -0700

    change on forms

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,7 +1,36 @@
+import {useState} from "react";
+
 const Login = () => {
+    const [data, setData] = useState(
+        {
+            email:"",
+            password:"", 
+        }
+    )
+
+    const loginUser = (e) =>{
+        e.preventDefault()
+    }
     return (
-        <div className="text-black">
-            Login
+        <div className="container mx-auto py-8">
+            <h1 className="text-2xl font-bold mb-6 text-center">Registration Form</h1>
+            <form className="w-full max-w-sm mx-auto bg-white p-8 rounded-md shadow-md" onSubmit={loginUser}>
+            <div className="mb-4">
+                    <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="email">Email</label>
+                    <input className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-indigo-500"
+                    type="email" id="email" name="email" placeholder="Please enter your email" value={data.email}
+                    onChange={(e)=>setData({...data, email:e.target.value})}/>
+                </div>
+                <div className="mb-4">
+                    <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="password">Password</label>
+                    <input className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-indigo-500"
+                    type="password" id="password" name="password" placeholder="********" value={data.password}
+                    onChange={(e)=>setData({...data, password:e.target.value})}/>
+                </div>
+                <button
+                    className="w-full bg-amber-300 text-white text-sm font-bold py-2 px-4 rounded-md hover:bg-amber-500 transition duration-300"
+                    type="submit">Login</button>
+            </form>
         </div>
     )
 }
--- src/pages/Signup/index.jsx
+++ src/pages/Signup/index.jsx
@@ -1,7 +1,46 @@
+import { useEffect, useState } from 'react';
+import { useForm } from 'react-hook-form';
+import { Link } from 'react-router-dom';
+import { useSearchParams, useNavigate } from 'react-router-dom';
+import { useAuth } from '../../hooks/useAuth';
+
+
 const Register = () => {
+    const [data, setData] = useState(
+        {
+            name:"",
+            email:"",
+            password:"", 
+        }
+    )
+  
+    
     return (
-        <div className="text-white">
-            Register
+        <div className="container mx-auto py-8">
+            <h1 className="text-2xl font-bold mb-6 text-center">Registration Form</h1>
+            <form className="w-full max-w-sm mx-auto bg-white p-8 rounded-md shadow-md" onSubmit={handleSubmit(submit)}>
+                <div className="mb-4">
+                    <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="name">Name</label>
+                    <input className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-indigo-500"
+                    type="text" id="name" name="name" placeholder="Please enter your name" value={data.name} 
+                    onChange={(e)=>setData({...data, name:e.target.value})}/>
+                </div>
+                <div className="mb-4">
+                    <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="email">Email</label>
+                    <input className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-indigo-500"
+                    type="email" id="email" name="email" placeholder="Please enter your email" value={data.email}
+                    onChange={(e)=>setData({...data, email:e.target.value})}/>
+                </div>
+                <div className="mb-4">
+                    <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="password">Password</label>
+                    <input className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-indigo-500"
+                    type="password" id="password" name="password" placeholder="********" value={data.password}
+                    onChange={(e)=>setData({...data, password:e.target.value})}/>
+                </div>
+                <button
+                    className="w-full bg-amber-300 text-white text-sm font-bold py-2 px-4 rounded-md hover:bg-amber-500 transition duration-300"
+                    type="submit">Register</button>
+            </form>
         </div>
     )
 }
